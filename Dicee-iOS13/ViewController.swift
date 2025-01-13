7a7c7212e71146520b1c47a614807e20987c738f
Daria Zakharova <daria.zakharova.work@gmail.com>
2025-01-13 14:12:27 +0100

    Add support for shake gesture

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,18 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
+    func changeDicee() {
         let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
         diceImageView1.image = diceArray.randomElement()
         diceImageView2.image = diceArray.randomElement()
     }
     
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        changeDicee()
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            changeDicee()
+        }
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.resignFirstResponder()
+    }
     
 }
 
